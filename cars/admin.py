c9faa21e33e53f99264540263210d4542529463c
Valdeilson Lima <valdeilsonbdl56@gmail.com>
2024-09-22 11:54:58 -0300

    Adiconando a tabela Brand e criando a chave estrangeira na tabela Cars

--- cars/admin.py
+++ cars/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from cars.models import car
+from cars.models import car, Brand
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model','brand')
     
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    
 admin.site.register(car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
