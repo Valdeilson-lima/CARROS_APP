3e04acee7b1c34cca757709604a898e62ac9fb57
Valdeilson Lima <valdeilsonbdl56@gmail.com>
2024-09-22 18:35:02 -0300

    Adicionando as views e os templestes

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from cars.models import car
+#from django.http import HttpResponse
 
-# Create your views here.
+def cars_view(request):
+    
+    
+    print(request.GET)
+    cars = car.objects.filter(model__contains='C')
+   
+    return  render(
+        request,
+        'cars.html', 
+        {'cars':cars } 
+    )
